633a5d197f92ec84c525340436d0e4ce0fd1b7bc
Mahery05 <mahery.razakandraina@ynov.com>
2020-12-11 21:07:05 +0100

    envoi du projet Javascript

---
+++ javascript/piege.js
@@ -0,0 +1,20 @@
+function tiragePiege(){
+    var degat;
+    var text="";
+	var rand=getRandomInt(10);
+	switch(rand){
+        case 0: 
+        degat=10;
+        joueur.hp=joueur.hp-degat;
+        text="💣 Vous êtes tombé dans un piège et avez perdu "+ degat+" hp il vous reste donc "+joueur.hp+" hp 💣<br/>";
+        break;
+        case 1:
+        degat=20;
+        joueur.hp=joueur.hp-degat;
+        text="💣 Vous êtes tombé dans un piège et avez perdu "+ degat+" hp il vous reste donc "+joueur.hp+" hp 💣<br/>";
+        break;
+  }
+  document.getElementById("dialog").innerHTML+=text;
+  joueur.joueurMort();
+  joueur.getCaracteristiques();
+}
